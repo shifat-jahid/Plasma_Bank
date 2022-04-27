1d21ee4cd31333e14098b6db809739a5a04bf3c6
shifat-jahid <mislam173065@bscse.uiu.ac.bd>
2022-04-28 00:45:08 +0600

    customer dashboard added

---
+++ customer_dashboard.php
@@ -0,0 +1,17 @@
+<!-- Header Start -->
+<?php
+require "includes/header.php";
+?>
+<!-- Header End -->
+
+<?php
+    require "dashboard_content/customer_dashboard_header.php";
+    require "dashboard_content/customer_dashboard_menu.php";
+?>
+
+
+<!-- footer start  -->
+<?php
+require "includes/footer.php";
+?>
+<!-- footer end  -->
