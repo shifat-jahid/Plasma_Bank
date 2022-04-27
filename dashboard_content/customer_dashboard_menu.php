f94f398e44ac805f888dcc6a57e4c25d35177bab
sumaiya-akter-mim <smim191029@bscse.uiu.ac.bd>
2022-04-28 00:18:40 +0600

    cutomer login added

---
+++ dashboard_content/customer_dashboard_menu.php
@@ -0,0 +1,21 @@
+<!-- Sidebar Start  -->
+<?php
+if (!isset($_SESSION)) {
+    session_start();
+}
+?>
+
+<div class="col-md-2 pl-0">
+    <div class="side-menu">
+        <nav>
+            <ul>
+                <li class="text-center"><a href="customer_add_post.php">Add Post</a></li>
+                <li class="text-center"><a href="customer_manage_post.php">Manage Post</a></li>
+                <li class="text-center"><a href="customer_dashboard_update_form.php">Update Profile</a></li>
+                <li class="text-center bg-danger mt-5"><a href="includes/logout.php">Logout</a></li>
+            </ul>
+        </nav>
+    </div>
+</div>
+
+<!-- Sidebar End  -->
